dc5fd03ecc2e8ce18a46a202530205403f726362
Engr.Ahsan Tariq <109543910+ahsantariq7@users.noreply.github.com>
2023-07-13 14:06:41 +0500

    Update settings.py

--- accounts/accounts/settings.py
+++ accounts/accounts/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'accounts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Key_Contact', 
+        'USER': 'postgres',
+        'PASSWORD': '0724',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
